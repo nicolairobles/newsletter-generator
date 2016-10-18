994e106f85513967210ff4eb04970cd08821ad35
Nicolai Robles <nicolai.robles@gmail.com>
2016-10-18 11:41:36 -0400

    added redesign template

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -6,12 +6,12 @@ const {
   }  = require('../db/db');
 
 
-// Display all Newsletters
+// GET: Display all Newsletters
 router.get('/', getAllNewsletters, (req,res) => {
   res.render('newsletter/index',{newsletters: res.rows});
 });
 
-// Post route to add new newsletter
+// POST to /new : post route to add new newsletter
 router.post('/new', addNewsletter, (req,res) => {
   newsletters = res.newsletters;
   // console.log(newsletters)
@@ -23,24 +23,49 @@ router.post('/new', addNewsletter, (req,res) => {
   res.redirect('/newsletter/'+length+'/edit');
 });
 
-// Show newsletter's profile
+// GET: Show newsletter's profile
 // router.get('/:id', getNewsletter, (req,res) => {
 //   res.render('newsletter/show', {newsletter: res.rows});
 // });
 
-// Show newsletter's articles and details
+
+// GET /edit: Page where you edit the newsletter
+router.get('/:id/edit', getNewsletter, (req,res) => {
+  console.log("in redirect request, before render")
+  res.render('newsletter/edit', {
+    newsletter: res.newsletter_details,
+    articles: res.articles
+  });
+});
+
+// PUT/UPDATE: Request with edited newsletter params
+router.put('/:id', updateNewsletter, (req,res) => {
+  res.redirect('/newsletter'+req.params.id);
+});
+
+// DELETE newsletter
+router.delete('/:id', deleteNewsletter, (req,res) => {
+ res.redirect('/newsletter');
+})
+
+
+
+
+// PAGES
+
+// NEWSWIRE: Show newsletter's articles and details
 router.get('/:id/newswire', getNewsletterDetails, (req,res) => {
   // res.set({ 'content-type': 'application/json; charset=utf-8' })
   res.render('newsletter/newswire', {
-  	newsletter_details: res.details,
-  	lead_article: res.lead_article,
-  	non_lead_articles: res.non_lead_articles,
-  	event: res.event
+    newsletter_details: res.details,
+    lead_article: res.lead_article,
+    non_lead_articles: res.non_lead_articles,
+    event: res.event
   });
   console.log(res.non_lead_articles)
 });
 
-// Show newsletter's articles and details
+// NEWSCENTER: Show newsletter's articles and details
 router.get('/:id/newscenter', getNewsletterDetails, (req,res) => {
   res.render('newsletter/newscenter', {
     newsletter_details: res.details,
@@ -51,24 +76,16 @@ router.get('/:id/newscenter', getNewsletterDetails, (req,res) => {
   // eval(pry.it)
 });
 
-// Page where you edit the newsletter
-router.get('/:id/edit', getNewsletter, (req,res) => {
-  console.log("in redirect request, before render")
-  res.render('newsletter/edit', {
-    newsletter: res.newsletter_details,
-    articles: res.articles
+// NEWSCENTER REDESIGN: Show newsletter's articles and details
+router.get('/:id/newscenter-new', getNewsletterDetails, (req,res) => {
+  res.render('newsletter/newscenter-new', {
+    newsletter_details: res.details,
+    lead_article: res.lead_article,
+    non_lead_articles: res.non_lead_articles,
+    event: res.event
   });
+  // eval(pry.it)
 });
-
-// Request with edited newsletter params
-router.put('/:id', updateNewsletter, (req,res) => {
-  res.redirect('/newsletter'+req.params.id);
-});
-
-// Delete newsletter
-router.delete('/:id', deleteNewsletter, (req,res) => {
- res.redirect('/newsletter');
-})
 
 
 
